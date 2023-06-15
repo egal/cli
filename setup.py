253f853d484ee699078bf1c68ab447e847b149d1
rollsover <51847757+rollsover@users.noreply.github.com>
2023-06-15 14:57:15 +0700

    ...

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-import os
-from dotenv import load_dotenv
-
 from setuptools import setup
 
 requirements = []
@@ -8,15 +5,8 @@ for require in open("requirements.txt", "r").read().split("\n"):
     if require != "":
         requirements.append(require)
 
-load_dotenv(dotenv_path=f"{os.getcwd()}/.env")
-
-version = os.getenv("VERSION")
-if version is None:
-    raise Exception("VERSION is not set")
-
 setup(
     name="egal-cli",
-    version="1.0",
     packages=["src"],
     include_package_data=True,
     install_requires=requirements,
